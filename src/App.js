c4ecc8c2252db7e28d00cdc691c2c6629c490b40
denervalspelizario <denervalspelizario@gmail.com>
2023-03-27 11:22:45 -0300

    update repositoriosApp criacao de estilizacao global com styled component e ja importado no app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 
 import Rotas from "./routes";
-
+import GlobalStyle from "./pages/styles/global";
 
 export default function App(){
 
 
   return(
-     <Rotas/>
+    <>                 {/* Para GlobalStyle precisa estar envolto de algo por isso o <> </>, não é recomendado usar div ficar atento a isso */}
+    <GlobalStyle/>
+    <Rotas/>  
+    </>
+     
   )
 }
 
