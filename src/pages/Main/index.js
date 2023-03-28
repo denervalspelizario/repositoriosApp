6e5d3d0fe70f8c64d5cfaeba041056064409f709
denervalspelizario <denervalspelizario@gmail.com>
2023-03-28 10:29:43 -0300

    update repositoriosApp estilizacao de main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,26 @@
 import React from 'react'
 
+import { Container, Form, SubmitButton } from './styles'
+import {FaGithub, FaPlus} from 'react-icons/fa' 
+
 export default function Main() {
   return (
-    <div>
-     <h1>Screen main</h1> 
+    <Container>
+      <h1>
+        <FaGithub />
+        Meus Repositorios
+      </h1> 
+      <Form>
+        <input  type='text' placeholder='adicionar repositorios' />
+
+        <SubmitButton>
+          <FaPlus   color='#FFF' size={14}  />
+        </SubmitButton>
+
+      </Form>
+
       
-    </div>
+    </Container>
   )
 }
 
