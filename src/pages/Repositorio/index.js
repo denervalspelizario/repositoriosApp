e8e302ecc2308b9e2eb4b8c06d12423a9226ee11
denervalspelizario <denervalspelizario@gmail.com>
2023-04-02 10:19:36 -0300

    update repositoriosApp criando funcao didimount que faz 2 requisicoes e adiciona elas em 2 states(repositorio e issues) e adicao de loading enquanto busca as requisicoes

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,9 +1,63 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
+import api from '../../services/api'
+import { Container, Owner, Loading } from './styles'
 
 export default function Repositorio({match}) {
+
+  const [repositorio, setRepositorio] = useState({}) // se inicia como objeto vazio pois receberá apenas 1 objeto
+  const [issues, setIssues] = useState([]) // se inicia como objeto vazio, pois recebera varios objetos 
+  const [loading, setLoading] = useState(true) 
+
+
+  // FUNCAO DIDMOUNT  QUE FAZ 2 REQUISIÇÕES(COM ALGUNS PARAMS) E ADICIONA NAS STATES REPOSITORIO E ISSUES 
+  useEffect(() => {
+
+    async function load(){
+  
+      const nomeRepo = decodeURIComponent(match.params.repositorio) // nomeRepo recebendo caminho do repositorio
+  
+      
+      const [repositorioData, issuesData] =  await Promise.all([ // o Promise fazendo DUAS requisições juntas e devolve dentro de uma array
+        api.get(`/repos/${nomeRepo}`), // req vai para repsoitorioData
+        
+        api.get(`/repos/${nomeRepo}/issues`, { // req vai para issuesData
+          params: { // params do axios
+            state:'open', // apenas os open
+            per_page: 5  // limitado a 5 por pagina
+          }
+        })
+       
+
+      ]);
+  
+      setRepositorio(repositorioData.data) // adicionando a state repositorio a requisição repositorioData
+      setIssues(issuesData.data) // adicionando a state issues a requisição issuesData
+      setLoading(false)
+    }
+    load() // chamando a funcao
+  
+  },[match.params.repositorio]) // 
+
+
+  if(loading){
+    return(
+      <Loading>
+        Carregando ....
+      </Loading>
+    )
+  }
   return (
-      <h1 style={{color: '#FFF'}}>
-          {decodeURIComponent(match.params.repositorio)}
-      </h1>
+      <Container>
+        <Owner>
+          <img 
+            src={repositorio.owner.avatar_url}  
+            alt={repositorio.owner.login} 
+          />
+          <h1>{repositorio.nome}</h1>
+          <p>{repositorio.description}</p>
+
+        </Owner>
+          
+      </Container>
   )
 }
--- src/pages/Repositorio/styles.js
+++ src/pages/Repositorio/styles.js
@@ -1,3 +1,23 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  color: #FFF;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+
+`;
+
+
+export const Owner = styled.header`
+  max-width: 700px;
+  background: #FFF;
+  border-radius: 4px;
+  box-shadow: 0 0 20px;
+  padding: 0 30px;
+  margin: 80px auto;
+
+`;
+
+export const Loading = styled.div``;
